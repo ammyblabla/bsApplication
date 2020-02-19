2ff9244a0daad152df628b82bb511b06e105b1ec
Natnaree Asavaseri <nasavas@ap.xom.com>
2020-02-19 21:36:10 +0700

    add eventhub template

--- bs/src/test/java/com/example/bs/BsSender.java
+++ bs/src/test/java/com/example/bs/BsSender.java
@@ -1,4 +1,11 @@
 package com.example.bs;
 
+
 public class BsSender {
+    private String connectionString = "";
+    private String eventHubName = "";
+    public static void main(String[] args) {
+
+    }
+
 }
