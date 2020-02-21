835a4f13b375435aae727283332009e605a82e9f
Natnaree Asavaseri <nasavas@ap.xom.com>
2020-02-21 23:47:02 +0700

    Trigger send data to eventhub via API

--- bs/src/main/java/com/example/bs/BsEventHubSender.java
+++ bs/src/main/java/com/example/bs/BsEventHubSender.java
@@ -2,49 +2,45 @@ package com.example.bs;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.microsoft.azure.eventhubs.*;
+import com.microsoft.azure.eventhubs.ConnectionStringBuilder;
+import com.microsoft.azure.eventhubs.EventData;
+import com.microsoft.azure.eventhubs.EventHubClient;
+import com.microsoft.azure.eventhubs.EventHubException;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.time.Instant;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
 public class BsEventHubSender {
-    public static void main(String[] args) throws IOException, EventHubException {
-        final ConnectionStringBuilder connStr = new ConnectionStringBuilder()
+    private EventHubClient ehClient;
+    private ScheduledExecutorService executorService;
+    private ConnectionStringBuilder connStr;
+
+    public BsEventHubSender() throws IOException, EventHubException {
+        connStr = new ConnectionStringBuilder()
                 .setNamespaceName("bus-application")
                 .setEventHubName("bus")
                 .setSasKeyName("bsProvider")
                 .setSasKey("dvZFFWy8L5/TXbB7H8C+/leGbWolh5ZO6C47LlNT2M8=");
-
-
-        final Gson gson = new GsonBuilder().create();
-        final ScheduledExecutorService executorService = Executors.newScheduledThreadPool(4);
-        final EventHubClient ehClient = EventHubClient.createFromConnectionStringSync(connStr.toString(), executorService);
-
-        try {
-            for (int i = 0; i < 100; i++) {
-
-//                String payload = "Message " + i;
-                BusDto payload = new BusDto("station", 3, 4);
-
-                byte[] payloadBytes = gson.toJson(payload).getBytes(Charset.defaultCharset());
-                EventData sendEvent = EventData.create(payloadBytes);
-
-                ehClient.sendSync(sendEvent);
-                System.out.println(payload);
-            }
-
-            System.out.println(Instant.now() + ": Send Complete...");
-            System.out.println("Press Enter to stop.");
-            System.in.read();
-        } finally {
-            ehClient.closeSync();
-            executorService.shutdown();
-        }
+        executorService = Executors.newScheduledThreadPool(4);
+        ehClient = EventHubClient.createFromConnectionStringSync(connStr.toString(), executorService);
     }
 
+    public void endConnection() throws EventHubException {
+        this.ehClient.closeSync();
+        this.executorService.shutdown();
+    }
+
+    public void sendData(BusDto payload) throws EventHubException {
+        final Gson gson = new GsonBuilder().create();
+        byte[] payloadBytes = gson.toJson(payload).getBytes(Charset.defaultCharset());
+        EventData sendEvent = EventData.create(payloadBytes);
+
+        this.ehClient.sendSync(sendEvent);
+        System.out.println(payload);
+
+    }
 }
 
 
--- bs/src/main/java/com/example/bs/BusController.java
+++ bs/src/main/java/com/example/bs/BusController.java
@@ -1,15 +1,18 @@
 package com.example.bs;
 
+import com.microsoft.azure.eventhubs.EventHubException;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.Random;
 
 @RestController
 public class BusController {
     @RequestMapping("/bus/{station}/{nr}")
-    public BusDto bus(@PathVariable String station, @PathVariable int nr) {
+    public BusDto sendBusToApi(@PathVariable String station, @PathVariable int nr) {
         int eta = getEtaBasedOnGpsAndOtherAdvancedStuff();
         BusDto b = new BusDto(station, nr, eta);
         return b;
@@ -20,5 +23,17 @@ public class BusController {
         int max = 7;
         int min = 1;
         return rn.nextInt(max - min +1) + min;
+    }
+
+    @RequestMapping("/eventhub/{station}/{nr}")
+    public void sendBusToEventHub(@PathVariable String station, @PathVariable int nr) throws IOException, EventHubException {
+        int eta = getEtaBasedOnGpsAndOtherAdvancedStuff();
+        BusDto bus = new BusDto(station, nr, eta);
+        BsEventHubSender bsEventHubSender = new BsEventHubSender();
+        try {
+            bsEventHubSender.sendData(bus);
+        } finally {
+            bsEventHubSender.endConnection();
+        }
     }
 }
