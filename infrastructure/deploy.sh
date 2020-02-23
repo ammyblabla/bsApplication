5c4ca201f382331e1e2fd702fc3da4b54df94620
Natnaree Asavaseri <nasavas@ap.xom.com>
2020-02-23 12:08:24 +0700

    Fix File Directory

--- infrastructure/deploy.sh
+++ infrastructure/deploy.sh
@@ -1,14 +1,13 @@
 echo "Please login before deployment"
 RESOURCE_GROUP="ExxonMobil_Sandbox"
+DIRECTORY=$(dirname $(realpath $0))
 az group deployment create \
   --name EventHubDeploy \
   --resource-group ${RESOURCE_GROUP} \
-  --template-file "${PWD}/eventHub/template.json"
+  --template-file "${DIRECTORY}/eventHub/template.json"
 
 az group deployment create \
   --name StorageAccountDeploy \
   --resource-group ${RESOURCE_GROUP} \
-  --template-file "${PWD}/storageAccount/template.json" \
-  --parameters "@${PWD}/storageAccount/parameters.json"
-
+  --template-file "${DIRECTORY}/storageAccount/template.json"
 
