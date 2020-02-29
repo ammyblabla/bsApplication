5adf700db559cc0178ff2e13ae99947a819f8f98
Natnaree Asavaseri <nasavas@ap.xom.com>
2020-02-29 21:32:19 +0700

    add consumer group

---
+++ bs/src/main/java/com/example/bs/EventHubController.java
@@ -0,0 +1,4 @@
+package com.example.bs;
+
+public class EventHubController {
+}
--- infrastructure/deploy.sh
+++ infrastructure/deploy.sh
@@ -1,11 +1,14 @@
 echo "Please login before deployment"
+az login
 RESOURCE_GROUP="ExxonMobil_Sandbox"
 DIRECTORY=$(dirname $(realpath $0))
+echo "Create Event Hub"
 az group deployment create \
   --name EventHubDeploy \
   --resource-group ${RESOURCE_GROUP} \
   --template-file "${DIRECTORY}/eventHub/template.json"
 
+echo "Create Storage Account"
 az group deployment create \
   --name StorageAccountDeploy \
   --resource-group ${RESOURCE_GROUP} \
